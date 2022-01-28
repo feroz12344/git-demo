a5890e889cf1d4d2697493c947fa97131a1515cf
feroz12344 <feroz.patel@humancloud.biz>
2022-01-27 22:16:00 -0800

    added if condition into class

--- src/main/java/com/example/GitDemo/GitDemoApplication.java
+++ src/main/java/com/example/GitDemo/GitDemoApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoApplication {
-	public void addData(){
+	public void addData(String deta){
 		System.out.println("adding data to repo");
+		if (deta.equals("java")){
+			System.out.println("welcome to ur system");
+		}
 	}
 
 	public static void main(String[] args) {
